4cc62404ea360e4584d1a1a301aa8fe3de41f020
Ashley Andrade <ashleyvitoriaandrade2004@gmail.com>
2026-06-11 21:47:26 -0300

    Salvamento das 5 quest finalizadas

--- js/script_questao05.js
+++ js/script_questao05.js
@@ -9,18 +9,17 @@ formDados.addEventListener('submit', (evt) => {
     const form_num = new FormData(formDados)
 
     
-    let vert1 = parseFloat(form_num.get('vert1'))
-    let vert2 = parseFloat(form_num.get('vert2'))
-    let vert3 = parseFloat(form_num.get('vert3'))
+    let vert1 = (form_num.get('vert1'))
+    let vert2 = (form_num.get('vert2'))
+    let vert3 = (form_num.get('vert3'))
 
 
 
 if (vert1 === vert2 && vert2 === vert3) {
-    divResultado.innerHTML = `Triângulo Equilátero`;
-
-} else if (vert1 === vert2 || vert1 === vert3 || vert2 === vert3) {
-    divResultado.innerHTML = `Triângulo Isósceles`;
-    
-} else {
-    divResultado.innerHTML = `Triângulo Escaleno`;
+    divResultado.innerHTML = (`Triângulo Equilátero`)
+}else if(vert1 === vert2 || vert1 === vert3 || vert2 === vert3) {
+    divResultado.innerHTML = (`Triângulo Isósceles`)
+}else{
+    divResultado.innerHTML = (`Triângulo Escaleno`)
 }
+})
